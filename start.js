bf33c889995338a9b9afcf2614e7bc3a8047e0cc
Yannick <yannick.dorn@gmail.com>
2019-03-31 18:24:19 -0400

    still toying around with getting the databse connected. After this it won't be very hard to get CRUD setup

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app= express();
+export const app= express();
 const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname+ '/index.html');
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
