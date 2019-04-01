2f43a66ac460bb7aa589cbd0b6bde32687b3a380
Yannick <yannick.dorn@gmail.com>
2019-04-01 00:28:16 -0400

    Setting up the rest of the db and defining Schemas

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const body-parser = require('body-parser');
+const app = express;
+const db = mongoose;
+
+var studentSchema = new db.Schema( {
+	Student : String,
+	id : String,
+	firstName : String,
+	lastName : String,
+	year : String,
+	age : String,
+	major : String,
+	gpa : String,
+});
